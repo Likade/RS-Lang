5ec715a27f10d1d572e3034dc50b20f36b5e9b46
Likade <lera24072@gmail.com>
2022-08-22 21:16:25 +0700

    feat: start of development of the registration/authorization system

---
+++ src/authorization/authorization.ts
@@ -0,0 +1,15 @@
+import { Component } from "../utils/component";
+import { UIButton } from "../utils/button";
+
+export class Authorization extends Component {
+    private authorizeBtn: UIButton;
+    private regiserBtn: UIButton;
+    private email: Component;
+    private password: Component;
+    private name: Component;
+    private avatar: Component;
+    constructor(parentNode: HTMLElement) {
+        super(parentNode, 'div', ['container']);
+        this.
+    }
+}
