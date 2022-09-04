e48918da0225e95b80f232559f5ed1b88bf0bc6f
Likade <lera24072@gmail.com>
2022-09-04 15:49:53 +0700

    try 2

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = ({ development }) => ({
   mode: development ? 'development' : 'production',
   devtool: development ? 'inline-source-map' : false,
   entry: {
-    main: './src/index.ts',
+    main: path.resolve(__dirname, "./src/"),
   },
   output: {
     filename: '[name].[contenthash].js',
